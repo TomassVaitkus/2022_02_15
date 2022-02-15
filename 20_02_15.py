a1a4dba368a6730d05793ece3bbc96ebf89682e9
Shliosen <tv.vaitkus@gmail.com>
2022-02-15 15:13:47 +0200

    pabandom pusshintt

---
+++ 20_02_15.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+with open('naujas_uzduociai.txt', 'a') as f:
+    f.write(str(pd.DataFrame([{i: str(open('uzduociai.txt', 'r').read()).count(i) for i in str(open('uzduociai.txt', 'r').read())}]).transpose().reset_index().rename({'index': 'Char', 0: 'Aperence'}, axis = 1).sort_values('Aperence').reset_index(drop=True)))
+    f.close()
